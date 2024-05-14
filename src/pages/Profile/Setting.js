08ebf8384077b5936a93927eef3f000229ba30c5
hamon1 <leejh021122@naver.com>
2024-05-14 17:17:03 +0900

    settingScreen - 로그아웃

--- src/pages/Profile/Setting.js
+++ src/pages/Profile/Setting.js
@@ -1,12 +1,38 @@
-import react from 'react';
-import {View, Text} from 'react-native';
+/**
+ * 설정 화면
+ *
+ * 로그아웃, 회원탈퇴
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
 
 const Setting = () => {
   return (
-    <View>
-      <Text>Setting</Text>
+    <View style={style.block}>
+      <Pressable style={style.item}>
+        <Text style={style.itemText}>로그아웃</Text>
+      </Pressable>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  block: {
+    flex: 1,
+    paddingTop: 32,
+  },
+  item: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColoer: '#eeeeee',
+    backgroundColor: 'white',
+    paddingVertical: 16,
+    paddingHorizontal: 16,
+  },
+  itemText: {
+    fontSize: 16,
+  },
+});
 
 export default Setting;
