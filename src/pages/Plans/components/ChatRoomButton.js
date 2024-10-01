9ff35189709d7c77f3b831a195bdd60097be71fa
hamon1 <leejh021122@naver.com>
2024-10-01 20:18:04 +0900

    그룹채팅 방 아이콘

--- src/pages/Plans/components/ChatRoomButton.js
+++ src/pages/Plans/components/ChatRoomButton.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
+import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 export const ChatRoomButton = ({chatRoomId, userId}) => {
     const navigate = useNavigation();
@@ -12,7 +14,9 @@ export const ChatRoomButton = ({chatRoomId, userId}) => {
                 style={styles.button}
                 onPress={() => navigate.navigate('chatScreen', { chatRoomId: chatRoomId, userId: userId })}
             >
-                <Text style={styles.text}>Chat Room</Text>
+                <IconMaterialCommunityIcons name="message" size={24} color="orange" />
+
+                {/* <Text style={styles.text}>Chat Room</Text> */}
             </TouchableOpacity>
         </View>
     )
